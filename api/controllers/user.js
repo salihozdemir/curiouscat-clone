4caffcfeb624ce6f5840c719398de604f7120e31
slhozdemir <salihozdemir94@gmail.com>
2019-12-29 13:40:06 +0300

    upload profile photo updated

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -145,7 +145,7 @@ exports.upload_profile_photo = (req, res, next) => {
     .then(result => {
       res.status(200).json({
         message: "PP updated",
-        ppUrl: result.profileImg,
+        profileImg: req.file.filename,
       });
     })
     .catch(err => {
