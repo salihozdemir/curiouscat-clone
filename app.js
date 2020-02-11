8766deb8eae144bba59f41f361a77a3a89c8cb55
Salih Ozdemir <salih.ozdemir@eltturkey.com>
2020-02-11 12:15:54 +0300

    static file path updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect(url,
 
 
 app.use(morgan("dev"));
-app.use(express.static('uploads'));
+app.use('/uploads' ,express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
