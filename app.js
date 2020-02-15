879cc9e376499fde05998aff5dc35145bd0da8c2
slhozdemir <salihozdemir94@gmail.com>
2020-02-15 12:39:08 +0300

    cors adress updated

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "askprivy.com");
+  res.header("Access-Control-Allow-Origin", "http://askprivy.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
