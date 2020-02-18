6a3d7378d0281774eef1c69c60df2a574ebef715
Salih Ozdemir <salih.ozdemir@eltturkey.com>
2020-02-18 16:13:28 +0300

    cors updated

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://askprivy.com");
-res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
